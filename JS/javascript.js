60e1344cbe7166b1a9e6fbbeba237200ad70db30
Cook-Amy <coo17045@byui.edu>
2019-09-27 17:48:11 -0400

    new pics

--- JS/javascript.js
+++ JS/javascript.js
@@ -18,9 +18,9 @@ function noseChoices() {
 }
 
 function mouthChoices() {
-  document.getElementById("picture1").src = "/Pics/mouth1.jpg";
-  document.getElementById("picture2").src = "/Pics/mouth2.jpg";
-  document.getElementById("picture3").src = "/Pics/mouth3.jpg";
+  document.getElementById("picture1").src = "/Pics/mouth1.png";
+  document.getElementById("picture2").src = "/Pics/mouth2.png";
+  document.getElementById("picture3").src = "/Pics/mouth3.png";
 }
 
 function randomSelection() {
