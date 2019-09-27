3bef16dd5948ab6a7c5fa129230e6034bd849810
Cook-Amy <coo17045@byui.edu>
2019-09-27 11:02:07 -0400

    functions added

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,5 +1,27 @@
-function pumpkinShape() {
+function pumpkinChoices() {
   document.getElementById("choice1").src = "/Pics/round-pumpkin.png";
   document.getElementById("choice2").src = "/Pics/tall-pumpkin.png";
   document.getElementById("choice3").src = "/Pics/short-pumpkin.png";
+}
+
+function eyeChoices() {
+  document.getElementById("choice1").src = "/Pics/square.png";
+  document.getElementById("choice2").src = "/Pics/square.png";
+  document.getElementById("choice3").src = "/Pics/square.png";
+}
+
+function noseChoices() {
+  document.getElementById("choice1").src = "/Pics/square.png";
+  document.getElementById("choice2").src = "/Pics/square.png";
+  document.getElementById("choice3").src = "/Pics/square.png";
+}
+
+function mouthChoices() {
+  document.getElementById("choice1").src = "/Pics/square.png";
+  document.getElementById("choice2").src = "/Pics/square.png";
+  document.getElementById("choice3").src = "/Pics/square.png";
+}
+
+function randomSelection() {
+  document.getElementById("result").src = "/Pics/square.png";
 }
