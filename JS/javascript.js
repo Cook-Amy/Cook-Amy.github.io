8848f6b60724b4bc806c87156967347c97236208
Cook-Amy <coo17045@byui.edu>
2019-09-27 12:13:42 -0400

    tyrying hidden pictures

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,25 +1,25 @@
 function pumpkinChoices() {
   document.getElementById("choice1").src = "/Pics/round-pumpkin.jpg";
-  document.getElementById("choice2").src = "/Pics/tall-pumpkin.png";
-  document.getElementById("choice3").src = "/Pics/short-pumpkin.png";
+  document.getElementById("choice2").src = "/Pics/tall-pumpkin.jpg";
+  document.getElementById("choice3").src = "/Pics/short-pumpkin.jpg";
 }
 
 function eyeChoices() {
-  document.getElementById("choice1").src = "/Pics/eyes1.png";
-  document.getElementById("choice2").src = "/Pics/eyes2.png";
-  document.getElementById("choice3").src = "/Pics/eyes3.png";
+  document.getElementById("choice1").src = "/Pics/eyes1.jpg";
+  document.getElementById("choice2").src = "/Pics/eyes2.jpg";
+  document.getElementById("choice3").src = "/Pics/eyes3.jpg";
 }
 
 function noseChoices() {
-  document.getElementById("choice1").src = "/Pics/nose1.png";
-  document.getElementById("choice2").src = "/Pics/nose2.png";
-  document.getElementById("choice3").src = "/Pics/nose3.png";
+  document.getElementById("choice1").src = "/Pics/nose1.jpg";
+  document.getElementById("choice2").src = "/Pics/nose2.jpg";
+  document.getElementById("choice3").src = "/Pics/nose3.jpg";
 }
 
 function mouthChoices() {
-  document.getElementById("choice1").src = "/Pics/mouth1.png";
-  document.getElementById("choice2").src = "/Pics/mouth2.png";
-  document.getElementById("choice3").src = "/Pics/mouth3.png";
+  document.getElementById("choice1").src = "/Pics/mouth1.jpg";
+  document.getElementById("choice2").src = "/Pics/mouth2.jpg";
+  document.getElementById("choice3").src = "/Pics/mouth3.jpg";
 }
 
 function randomSelection() {
