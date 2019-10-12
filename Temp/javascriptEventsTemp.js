ec9ec47c87f31e159b96c78047a02c050b451950
Cook-Amy <coo17045@byui.edu>
2019-10-12 19:41:28 -0400

    puppy page

---
+++ Temp/javascriptEventsTemp.js
@@ -0,0 +1,81 @@
+/*************************************************
+ * Puppy Pictures
+ * ontouch events. Change the picture when it is 
+ * touched. Change back when finger is let go.
+*************************************************/
+function changePic(num) {
+  switch (num) {
+    case 1:
+      document.getElementById("dogPic1").src = "bulldogAd.jpg";
+      break;
+    case 2:
+      document.getElementById("dogPic2").src = "bernieAd.jpg";
+      break;
+    case 3:
+      document.getElementById("dogPic3").src = "chowAd.jpg";
+      break;
+    case 4:
+      document.getElementById("dogPic4").src = "bassettAd.jpg";
+      break;
+    case 5:
+      document.getElementById("dogPic5").src = "goldenRetrieverAd.jpg";
+      break;
+    case 6:
+      document.getElementById("dogPic6").src = "huskyAd.jpg";
+      break;
+    case 7:
+      document.getElementById("dogPic7").src = "irishSetterAd.jpg";
+      break;
+    case 8:
+      document.getElementById("dogPic8").src = "pugAd.jpg";
+      break;
+    case 9:
+      document.getElementById("dogPic9").src = "pomeranianAd.jpg";
+      break;
+    case 10:
+      document.getElementById("dogPic10").src = "boxerAd.jpg";
+      break;
+    case 11:
+      document.getElementById("dogPic11").src = "beagleAd.jpg";
+      break;
+    case 12:
+      document.getElementById("dogPic12").src = "poodleAd.jpg";
+      break;
+    case 13:
+      document.getElementById("dogPic1").src = "bulldogPup.jpg";
+      break;
+    case 14:
+      document.getElementById("dogPic2").src = "berniePup.jpg";
+      break;
+    case 15:
+      document.getElementById("dogPic3").src = "chowPup.jpg";
+      break;
+    case 16:
+      document.getElementById("dogPic4").src = "bassettPup.jpg";
+      break;
+    case 17:
+      document.getElementById("dogPic5").src = "goldenRetrieverPup.jpg";
+      break;
+    case 18:
+      document.getElementById("dogPic6").src = "huskyPup.jpg";
+      break;
+    case 19:
+      document.getElementById("dogPic7").src = "irishSetterPup.jpg";
+      break;
+    case 20:
+      document.getElementById("dogPic8").src = "pugPup.jpg";
+      break;
+    case 21:
+      document.getElementById("dogPic9").src = "pomeranianPup.jpg";
+      break;
+    case 22:
+      document.getElementById("dogPic10").src = "boxerPup.jpg";
+      break;
+    case 23:
+      document.getElementById("dogPic11").src = "beaglePup.jpg";
+      break;
+    case 24:
+      document.getElementById("dogPic12").src = "poodlePup.jpg";
+      break;
+  }
+}
